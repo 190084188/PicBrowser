0144a5a27bf19805a529e2b926ddf169fc2f1b5c
Lismoon <1647546850@qq.com>
2023-09-28 12:57:51 +0800

    更新了图像处理中的图像腐蚀和膨胀的自写函数

--- 3.py
+++ 3.py
@@ -1,25 +1,33 @@
 import cv2
 import numpy as np
-points = []
-img = cv2.imread('1.jpg')
-def click_event(event, x, y, flags, param):
-    imgcopy = img.copy()
-    if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
-        points.append((x, y))
-        if len(points) == 4:
-            cv2.destroyAllWindows()
-
-
-cv2.imshow('image', img)
-h,w,c=img.shape
-cv2.setMouseCallback('image', click_event)
-cv2.waitKey(0)
-pts1 = np.float32(points)
-pts2 = np.float32([[0,0],[w,0],[0,h],[w,h]])
-
-M = cv2.getPerspectiveTransform(pts1,pts2)
-result = cv2.warpPerspective(img,M,(w,h))
-cv2.imshow('dst',result)
+# 读取图片
+img = cv2.imread('chair.bmp')
+# 灰度图
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 边缘检测
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+# BGR -> RGB
+img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_Show = img_RGB.copy()
+# 霍夫直线变化
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 160)
+# 绘制检测到的直线
+for line in lines:
+    # (r, θ)
+    rho, theta = line[0]
+    # 通过(r, θ)获取到(x0,y0)
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    # 获得(x1,y1),(x2,y2)
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * a)
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * a)
+    # 绘制直线
+    cv2.line(img_RGB, (x1, y1), (x2, y2), (255, 0, 0), 2)
+# 显示图片
+cv2.imshow('result', img_RGB)
 cv2.waitKey()
 cv2.destroyAllWindows()
--- erosion_and_dilate.py
+++ erosion_and_dilate.py
@@ -1,17 +1,18 @@
 import cv2
 import numpy as np
+
 # 读取图片
 src = cv2.imread('fish.bmp')
 src = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
 
-# 设置卷积核
+# 设置卷积核,size定义的是卷积核的大小，types是卷积核的类型（均值卷积核和十字），value是结构元素的值
 def set_kernel(size, types=1, value=255):
-    value_inf = 255 - value
-    if types == 1:
+    value_inf = 255 - value  # 背景
+    if types == 1:  # 均值卷积核
         k = np.zeros((size, size), np.uint8)
         k.fill(value)
-    elif types == 2:
+    elif types == 2:  # 十字卷积核
         k = np.zeros((size, size), np.uint8)
         k.fill(value_inf)
         if size % 2 != 0:
@@ -24,44 +25,51 @@ def set_kernel(size, types=1, value=255):
 
 
 def get_element_position(matrix, number):
-    position = []
-    for i, row in enumerate(matrix):
+    position = []  # 把位置存入列表中
+    for i, row in enumerate(matrix):  # 对矩阵进行枚举，返回结构元素的位置
         for j, value in enumerate(row):
             if value == number:
                 position.append([i, j])
     return position
 
 
+# 传入待处理图片，卷积核和ROI的灰度值
 def my_erode(img, kernel, value):
-    structure_position = get_element_position(kernel,value)
+    # 先得到结构元素位置
+    structure_position = get_element_position(kernel, value)
+    # 设定匹配标志
     match = 0
+    # 背景色
     value_inf = 255 - value
     h, w = img.shape
     k_size = len(kernel)
     out = img.copy()
     for i in range(h - 1):
         for j in range(w - 1):
+            # 图像四边处理为背景色
             if i + 1 < k_size / 2 or j + 1 < k_size / 2 or h - 1 - i < k_size / 2 or w - 1 - j < k_size / 2:
                 out[i, j] = value_inf
             else:
+                # 得到roi区域的图像矩阵，大小和卷积核一样
                 x_start = i - k_size // 2
-                x_end = i + k_size // 2+1
+                x_end = i + k_size // 2 + 1
                 y_start = j - k_size // 2
-                y_end = j + k_size // 2+1
-                roi = img[x_start:x_end,y_start:y_end]
+                y_end = j + k_size // 2 + 1
+                roi = img[x_start:x_end, y_start:y_end]
                 for structures in structure_position:
-                    if roi[structures[0],structures[1]] == value:
+                    if roi[structures[0], structures[1]] == value:
                         match += 1
+                # 如果全匹配则保留，否则为背景色
                 if match == len(structure_position):
-                    out[i,j]=value
+                    out[i, j] = value
                 else:
-                    out[i,j]=value_inf
+                    out[i, j] = value_inf
             match = 0
     return out
 
 
-def my_dilate(img,kernel,value):
-    structure_position = get_element_position(kernel,value)
+def my_dilate(img, kernel, value):
+    structure_position = get_element_position(kernel, value)
     value_inf = 255 - value
     h, w = img.shape
     k_size = len(kernel)
@@ -72,10 +80,10 @@ def my_dilate(img,kernel,value):
                 out[i, j] = value_inf
             else:
                 x_start = i - k_size // 2
-                x_end = i + k_size // 2+1
+                x_end = i + k_size // 2 + 1
                 y_start = j - k_size // 2
-                y_end = j + k_size // 2+1
-                roi = img[x_start:x_end,y_start:y_end]
+                y_end = j + k_size // 2 + 1
+                roi = img[x_start:x_end, y_start:y_end]
                 for m, n in structure_position:
                     if roi[m, n] == value:
                         out[i, j] = value  # 如果有匹配就为255
@@ -83,14 +91,22 @@ def my_dilate(img,kernel,value):
     return out
 
 
-kernel = set_kernel(3, 2, 0)
-# 图像腐蚀处理
-erosion = my_erode(src, kernel,0)
-dilate = my_dilate(src,kernel,0)
-# 显示图像
-cv2.imshow("erosion", erosion)
-cv2.imshow("dilate",dilate)
-# 等待显示
-cv2.imshow("src", src)
+# img = cv2.imread("fish.bmp", cv2.IMREAD_GRAYSCALE)
+# v1 = cv2.Canny(img, 100, 200)  # 80为minVal(越小标准越低), 150为maxVal（越小标准越低）
+# cv2.imshow('res', v1)
+# kernel = set_kernel(3, 2, 0)
+# ero_cv = cv2.erode(src, kernel)
+# dilate_cv =cv2.dilate(src, kernel)
+# # 图像腐蚀处理
+# erosion = my_erode(src, kernel, 0)
+# dilate = my_dilate(src, kernel, 0)
+# # 显示图像
+# cv2.imshow("erosion", erosion)
+# cv2.imshow("dilate", dilate)
+# cv2.imshow("erosion_cv", ero_cv)
+# cv2.imshow("dilate_cv", dilate_cv)
+# # 等待显示
+# cv2.imshow("src", src)
+chair = cv2.imread("chair.bmp")
 cv2.waitKey(0)
 cv2.destroyAllWindows()
